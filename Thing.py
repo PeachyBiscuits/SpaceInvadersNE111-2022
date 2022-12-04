5ae75e036545c3821090d1c2597369651725ee14
ejhann <ethanatangan@gmail.com>
2022-12-04 16:28:35 -0500

    Thing added

---
+++ Thing.py
@@ -0,0 +1,12 @@
+import pygame
+
+class ship:
+    health = 3 
+    speed = 5  #this is arbitrary idk what to put 
+
+    #add image thingy 
+class alien:
+    health = 1 
+    speed = 5 #this is arbitrary idk what to put 
+
+    #add image thingy
--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 import pygame
+import math
 
-print("we should work on this project")
+class ship:
+    health = 3 
+    speed = 5  #this is arbitrary idk what to put 
+
+    #add image thingy 
+class alien:
+    health = 1 
+    speed = 5 #this is arbitrary idk what to put 
+
+    #add image thingy
+
